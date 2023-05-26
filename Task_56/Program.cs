2f740b298167547ebf1867e7cea9d0c534b327f1
Dmitriy <mitya.marinin11@gmail.com>
2023-05-26 13:22:08 +0700

    task56

--- Task_56/Program.cs
+++ Task_56/Program.cs
@@ -65,13 +65,3 @@ void WriteArray(int[,] array)
         Console.WriteLine();
     }
 }
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
